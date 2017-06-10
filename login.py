37d2ca57ab4f32da4b64c1e536d713fdcda76167
ahn <93immm@naver.com>
2017-06-09 21:06:02 -0700

    version 2 / 소스 최적화

--- login.py
+++ login.py
@@ -20,13 +20,9 @@ data = {
 endata = urllib.urlencode(data)
 request = urllib2.Request(url, endata)
 response = urllib2.urlopen(request)
-
 cookie = response.headers.get('Set-Cookie')
-
 print cookie
-
 realcookie = "'" + cookie[10:42] + "'"
-
 print realcookie
 
 #------------ GET FORM DATA ------------#
@@ -34,55 +30,27 @@ print ("========================================================================
 url="http://gall.dcinside.com/board/delete/?id=japanese&no=" + noticeNUM
 cookies = {'PHPSESSID':sessionID,'ci_c':csrfToken,'notice_no':noticeNUM}
 con = requests.get(url, data=data, cookies=cookies).text 
-
 soup = BeautifulSoup(con, "lxml")
 link = soup.find_all("input")
-
-# DEBUG
-#for kk in range(3,len(link)):
-#    print link[kk]
-#print ("==================================================================================================")
-
 for nn in range(4,len(link)-1):
     try:
         tmp = link[nn]
-        # DEBUG
-        #print "{}={}".format(tmp['name'], tmp['value'])
         payload[tmp['name']] = tmp['value']
     except BaseException:
         pass
-        # DEBUG
-        #print "{}=".format(tmp['name'])
-
 link = soup.find_all("script")
-
 strlink = str(link[28])
 lastindex = strlink.find('formData += "&')
-# DEBUG
-#print strlink[lastindex+14:lastindex+95]
 
 #------------ MAKE PAYLOAD ------------#
-#print ("==================================================================================================")
-
 jsform = strlink[lastindex+14:lastindex+95]
 jsformKey = strlink[lastindex+14:lastindex+54]
 jsformVal = strlink[lastindex+55:lastindex+95]
-
-#print jsform
-#print jsformKey
-#print jsformVal
-
 payload[jsformKey] = jsformVal
-
-# DEBUG
-#print payload
-#print ("==================================================================================================")
 
 #------- DELETE REQUEST -------#
 url="http://gall.dcinside.com/forms/delete_submit"
-
 cookies = {'PHPSESSID':sessionID,'ci_c':csrfToken,'notice_no':noticeNUM}
 headers = {'X-Requested-With':'XMLHttpRequest'}
-
 con = requests.post(url, data=payload, cookies=cookies, headers=headers)
 print con
